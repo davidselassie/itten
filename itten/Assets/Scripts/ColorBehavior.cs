2c1cb6dc3b0d5f51c2de5af516f2aaad1cc4f67c
David Selassie <selassid@gmail.com>
2015-04-15 11:06:16 -0700

    Adds color changing test.

---
+++ itten/Assets/Scripts/ColorBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class ColorBehavior : MonoBehaviour {
+    public enum ColorEnum {
+        Blue,
+        Red
+    };
+
+	public ColorEnum Color = ColorEnum.Blue;
+    public Collider2D Collider2D {
+        get;
+        private set;
+    }
+	private Renderer Renderer;
+
+    void Start () {
+        Collider2D = GetComponent<Collider2D>();
+		Renderer = GetComponent<Renderer>();
+		ReckonColorChange ();
+    }
+
+	public void ReckonColorChange () {
+		AllowCollisions ();
+		if (Color == ColorEnum.Blue) {
+			Renderer.material.color = UnityEngine.Color.blue;
+		} else {
+			Renderer.material.color = UnityEngine.Color.red;
+		}
+	}
+
+    private void AllowCollisions () {
+        ColorBehavior[] CBs = FindObjectsOfType(typeof(ColorBehavior)) as ColorBehavior[];
+        foreach (ColorBehavior CB in CBs) {
+            Physics2D.IgnoreCollision (Collider2D,
+                                       CB.Collider2D,
+                                       !ShouldCollide (Color, CB.Color));
+        }
+    }
+
+    private static bool ShouldCollide (ColorEnum one, ColorEnum two) {
+        return one != two;
+    }
+}
---
+++ itten/Assets/Scripts/ColorSwapController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ColorBehavior))]
+public class ColorSwapController : MonoBehaviour {
+	private ColorBehavior colorBehavior;
+
+	void Start () {
+		colorBehavior = GetComponent<ColorBehavior>();
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown ("space")) {
+			Swap ();
+		}
+	}
+
+	void Swap () {
+		if (colorBehavior.Color == ColorBehavior.ColorEnum.Blue) {
+			colorBehavior.Color = ColorBehavior.ColorEnum.Red;
+		} else {
+			colorBehavior.Color = ColorBehavior.ColorEnum.Blue;
+		}
+		Debug.Log ("Color swapped", colorBehavior);
+		colorBehavior.ReckonColorChange ();
+	}
+}
