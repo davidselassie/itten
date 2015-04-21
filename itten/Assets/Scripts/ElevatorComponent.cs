cf9514cbda4265fe8cf823a601327b81aad534ee
David Selassie <selassid@gmail.com>
2015-04-21 14:38:39 -0700

    Adds sin elevator component.

---
+++ itten/Assets/Scripts/ElevatorComponent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ElevatorComponent : MonoBehaviour {
+	void FixedUpdate () {
+		gameObject.transform.Translate(new Vector2(0.0f, (float)Math.Sin(Time.fixedTime) / 50.0f));
+	}
+}
