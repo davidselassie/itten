85bf6ae07c82657c8c28eb9a47b0af6da500e234
Cosmo Ray <cosmor@gmail.com>
2015-04-21 22:19:38 -0700

    Color Shift Burst Script

---
+++ itten/Assets/Scripts/ColorShiftBurst.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorShiftBurst : MonoBehaviour {
+
+	public GameObject burstPrefab;
+	public Transform burstTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("m")) 
+		{
+			GameObject burstInstance;
+			burstInstance = Instantiate(burstPrefab, burstTarget.position, burstTarget.rotation) as GameObject;
+			burstInstance.transform.parent = transform;
+		}
+	}
+}
