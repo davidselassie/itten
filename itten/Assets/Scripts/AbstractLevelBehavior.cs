9e412de2cff26dd01b657c9e40b81518cf20d368
David Selassie <selassid@gmail.com>
2015-04-24 16:13:52 -0700

    Adds restart and advance sounds.

--- itten/Assets/Scripts/AbstractLevelBehavior.cs
+++ itten/Assets/Scripts/AbstractLevelBehavior.cs
@@ -4,8 +4,6 @@ using System.Collections;
 [RequireComponent(typeof(Collider2D))]
 public abstract class AbstractLevelBehavior : MonoBehaviour {
 	protected GameFlowLogic FlowLogic;
-
-	private AudioSource Audio;
 	
 	void Start () {
 		Collider2D collider = GetComponent<Collider2D>();
@@ -16,15 +14,11 @@ public abstract class AbstractLevelBehavior : MonoBehaviour {
 		if (FlowLogic == null) {
 			Debug.LogWarning ("No GameFlowLogic in scene. Level behavior collision will restart level.");
 		}
-		Audio = GetComponent<AudioSource>();
 	}
 	
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			if (FlowLogic != null) {
-				if (Audio != null) {
-					Audio.Play ();
-				}
 				Action ();
 			} else {
 				Application.LoadLevel(Application.loadedLevel);
