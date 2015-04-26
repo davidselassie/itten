60cd6f932dba05498a2e07b6433e6db9d7a452d2
Cosmo Ray <cosmor@gmail.com>
2015-04-26 15:43:55 -0700

    Texture tiler!

---
+++ itten/Assets/Scripts/AutoTileTexture.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AutoTileTexture : MonoBehaviour {
+
+	public float tileScale = 2.0f;
+
+
+	void OnDrawGizmos()
+	{
+		
+		this.gameObject.GetComponent<Renderer>().sharedMaterial.SetTextureScale("_MainTex",new Vector2(this.gameObject.transform.lossyScale.x * tileScale,this.gameObject.transform.lossyScale.y * tileScale))  ;
+	}
+}
